8e0498e55e89c84edeebd68a1a6d36d91b6f4735
Ernesto Andres Rincon Cruz <ernesto.arc@gmail.com>
2021-08-24 20:53:43 -0500

    Ejemplo1: tipos de datos ANSI-C

--- K32L2B31A_Project/source/K32L2B31A_Project.c
+++ K32L2B31A_Project/source/K32L2B31A_Project.c
@@ -1,37 +1,16 @@
-/*
- * Copyright 2016-2021 NXP
- * All rights reserved.
+/*! @file : K32L2B31A_Project.c
+ * @author  Ernesto Andres Rincon Cruz
+ * @version 0.0.000
+ * @date    23/08/2021
+ * @brief   Funcion principal main
+ * @details
+ *			v0.0.000	Proyecto base creado usando MCUXpresso
  *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
  *
- * o Redistributions of source code must retain the above copyright notice, this list
- *   of conditions and the following disclaimer.
- *
- * o Redistributions in binary form must reproduce the above copyright notice, this
- *   list of conditions and the following disclaimer in the documentation and/or
- *   other materials provided with the distribution.
- *
- * o Neither the name of NXP Semiconductor, Inc. nor the names of its
- *   contributors may be used to endorse or promote products derived from this
- *   software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
-/**
- * @file    K32L2B31A_Project.c
- * @brief   Application entry point.
- */
+/*******************************************************************************
+ * Includes
+ ******************************************************************************/
 #include <stdio.h>
 #include "board.h"
 #include "peripherals.h"
@@ -39,15 +18,28 @@
 #include "clock_config.h"
 #include "K32L2B31A.h"
 #include "fsl_debug_console.h"
-/* TODO: insert other include files here. */
+#include <limits.h>
+#include <float.h>
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
 
-/* TODO: insert other definitions and declarations here. */
+/*******************************************************************************
+ * Private Prototypes
+ ******************************************************************************/
 
-/*
- * @brief   Application entry point.
- */
+/*******************************************************************************
+ * External vars
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Local vars
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Private Source Code
+ ******************************************************************************/
 int main(void) {
-
     /* Init board hardware. */
     BOARD_InitBootPins();
     BOARD_InitBootClocks();
@@ -57,7 +49,50 @@ int main(void) {
     BOARD_InitDebugConsole();
 #endif
 
-    PRINTF("Hello World\n");
+    printf("============================================================\r\n");
+	printf("Size of C data types:\r\n");
+	printf("Type               Bytes\r\n");
+	printf("--------------------------------\r\n");
+	printf("char                 %lu\r\n", sizeof(char));
+	printf("int8_t               %lu\r\n", sizeof(int8_t));
+	printf("unsigned char        %lu\r\n", sizeof(unsigned char));
+	printf("uint8_t              %lu\r\n", sizeof(uint8_t));
+	printf("short                %lu\r\n", sizeof(short));
+	printf("int16_t              %lu\r\n", sizeof(int16_t));
+	printf("uint16t              %lu\r\n", sizeof(uint16_t));
+	printf("int                  %lu\r\n", sizeof(int));
+	printf("unsigned             %lu\r\n", sizeof(unsigned));
+	printf("long                 %lu\r\n", sizeof(long));
+	printf("unsigned long        %lu\r\n", sizeof(unsigned long));
+	printf("int32_t              %lu\r\n", sizeof(int32_t));
+	printf("uint32_t             %lu\r\n", sizeof(uint32_t));
+	printf("long long            %lu\r\n", sizeof(long long));
+	printf("int64_t              %lu\r\n", sizeof(int64_t));
+	printf("unsigned long long   %lu\r\n", sizeof(unsigned long long));
+	printf("uint64_t             %lu\r\n", sizeof(uint64_t));
+	printf("float                %lu\r\n", sizeof(float));
+	printf("double               %lu\r\n", sizeof(double));
+	printf("long double          %lu\r\n", sizeof(long double));
+	printf("_Bool                %lu\r\n", sizeof(_Bool));
+	printf("============================================================\r\n");
+	printf("Ranges for integer data types in C\r\n");
+	printf("------------------------------------------------------------\r\n");
+	printf("int8_t    %20d  %20d\r\n", SCHAR_MIN, SCHAR_MAX);
+	printf("int16_t   %20d  %20d\r\n", SHRT_MIN, SHRT_MAX);
+	printf("int32_t   %20d  %20d\r\n", INT_MIN, INT_MAX);
+	printf("int64_t   %20lld  %20lld\r\n", LLONG_MIN, LLONG_MAX);
+	printf("uint8_t   %20d  %20d\r\n", 0, UCHAR_MAX);
+	printf("uint16_t  %20d  %20d\r\n", 0, USHRT_MAX);
+	printf("uint32_t  %20d  %20u\r\n", 0, UINT_MAX);
+	printf("uint64_t  %20d  %20llu\r\n", 0, ULLONG_MAX);
+	printf("\r\n");
+	printf("============================================================\r\n");
+	printf("Ranges for real number data types in C\r\n\r\n");
+	printf("------------------------------------------------------------\r\n");
+	printf("float        %14.7g  %14.7g\r\n", FLT_MIN, FLT_MAX);
+	printf("double       %14.7g  %14.7g\r\n", DBL_MIN, DBL_MAX);
+	printf("long double  %14.7Lg  %14.7Lg\r\n", LDBL_MIN, LDBL_MAX);
+	printf("\r\n");
 
     /* Force the counter to be placed into memory. */
     volatile static int i = 0 ;
