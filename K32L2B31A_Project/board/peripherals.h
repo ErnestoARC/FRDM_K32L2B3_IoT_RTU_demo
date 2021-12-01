ec29a8e840e2246de72a6595f73a64d2269275e7
Ernesto Andres Rincon Cruz <ernesto.arc@gmail.com>
2021-11-30 20:38:36 -0500

    Inicializacion automática de LPUART0

--- K32L2B31A_Project/board/peripherals.h
+++ K32L2B31A_Project/board/peripherals.h
@@ -12,6 +12,8 @@
 #include "fsl_common.h"
 #include "fsl_lptmr.h"
 #include "fsl_adc16.h"
+#include "fsl_lpuart.h"
+#include "fsl_clock.h"
 
 #if defined(__cplusplus)
 extern "C" {
@@ -29,9 +31,9 @@ extern "C" {
 /* Definition of the prescaled clock source frequency */
 #define LPTMR0_INPUT_FREQ 1000UL
 /* Definition of the timer period in us */
-#define LPTMR0_USEC_COUNT 1000000UL
+#define LPTMR0_USEC_COUNT 100000UL
 /* Definition of the timer period in number of ticks */
-#define LPTMR0_TICKS 1000UL
+#define LPTMR0_TICKS 100UL
 /* LPTMR0 interrupt vector ID (number). */
 #define LPTMR0_IRQN LPTMR0_IRQn
 /* LPTMR0 interrupt handler identifier. */
@@ -54,6 +56,14 @@ extern "C" {
 #define ADC0_CH1_CONTROL_GROUP 0
 /* Channel 2 (SE.26) conversion control group. */
 #define ADC0_CH2_CONTROL_GROUP 0
+/* Definition of peripheral ID */
+#define LPUART0_PERIPHERAL LPUART0
+/* Definition of the clock source frequency */
+#define LPUART0_CLOCK_SOURCE 48000000UL
+/* LPUART0 interrupt vector ID (number). */
+#define LPUART0_SERIAL_RX_TX_IRQN LPUART0_IRQn
+/* LPUART0 interrupt handler identifier. */
+#define LPUART0_SERIAL_RX_TX_IRQHANDLER LPUART0_IRQHandler
 
 /***********************************************************************************************************************
  * Global variables
@@ -62,6 +72,7 @@ extern const lptmr_config_t LPTMR0_config;
 extern adc16_channel_config_t ADC0_channelsConfig[3];
 extern const adc16_config_t ADC0_config;
 extern const adc16_channel_mux_mode_t ADC0_muxMode;
+extern const lpuart_config_t LPUART0_config;
 
 /***********************************************************************************************************************
  * Initialization functions
